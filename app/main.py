b1b04cb233b766a3cfb17dd0f1244b0c83f05114
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2021-10-31 19:43:51 +0900

    mainからDB操作設計（Create, Readのみ）

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from typing import Dict
-
-app = FastAPI()
-
-
-@app.get("/")
-def hello() -> Dict[str, str]:
-    return {"message": "hello world!"}
---
+++ sql_app/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from typing import Dict, List
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine) 
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#DB_CREATE
+@app.post("/users", response_model=schemas.User)
+async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
+
+@app.post("/events", response_model=schemas.Event)
+async def create_event(event: schemas.event, db: Session = Depends(get_db)):
+    return crud.create_event(db=db, event=event)
+
+#DB_READ
+@app.get("/users", response_model=List[schemas.User])
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@app.get("/events", response_model=List[schemas.Event])
+async def read_events(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    events = crud.get_events(db, skip=skip, limit=limit)
+    return events    
+
+
+@app.get("/")
+def hello() -> Dict[str, str]:
+    return {"message": "hello world!"}
