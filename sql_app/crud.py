66e4843aff421546ea61369a7ce0e1b3969b8d67
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2021-11-01 17:02:39 +0900

    file modified ver.1

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,37 +1,42 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
-##ユーザー情報取得
-def get_users(db: Session, skip: int = 0, limit: int = 100):
+
+# ユーザー情報取得
+def get_users(db: Session, skip: int=0, limit: int=100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-##イベント情報取得
-def get_events(db: Session, skip: int = 0, limit: int = 100):
+
+# イベント情報取得
+def get_events(db: Session, skip: int=0, limit: int=100):
     return db.query(models.Event).offset(skip).limit(limit).all()
 
-#ユーザー登録
+
+# ユーザー登録
 def create_user(db: Session, user: schemas.User):
-    db_user = models.User(user_name     = user.user_name, 
-                          user_pass     = user.password_hash,
-                          user_email    = user.email,
-                          user_image    = user.profile_image_path,
+    db_user = models.User(user_name = user.user_name, 
+                          user_pass = user.password_hash,
+                          user_email = user.email,
+                          user_image = user.profile_image_path,
                           user_birthday = user.birthday)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
-#イベント登録
+
+# イベント登録
 def create_event(db: Session, event: schemas.Event):
-    db_event = models.event(event_name        = event.event_name, 
-                            event_description = event.description,
-                            event_begin_date  = event.begin_date,
-                            event_is_allday   = event.is_all_day,
-                            event_end_date    = event.end_date,
-                            event_created_at  = event.created_at,
-                            event_updated_at  = event.updated_at,
-                            event_deleted_at  = event.deleted_at,
-                            event_color       = event.color)
+    db_event = models.event(event_name = event.event_name, 
+    event_description = event.description, 
+    event_begin_date = event.begin_date, 
+    event_is_allday = event.is_all_day, 
+    event_end_date = event.end_date, 
+    event_created_at = event.created_at, 
+    event_updated_at = event.updated_at, 
+    event_deleted_at = event.deleted_at, 
+    event_color = event.color)
+
     db.add(db_event)
     db.commit()
     db.refresh(db_event)
--- sql_app/main.py
+++ sql_app/main.py
@@ -5,7 +5,7 @@ from typing import Dict, List
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
-models.Base.metadata.create_all(bind=engine) 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -16,25 +16,29 @@ def get_db():
     finally:
         db.close()
 
-#DB_CREATE
+
+# DB_CREATE
 @app.post("/users", response_model=schemas.User)
-async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+async def create_user(user: schemas.User, db: Session=Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
+
 @app.post("/events", response_model=schemas.Event)
-async def create_event(event: schemas.event, db: Session = Depends(get_db)):
+async def create_event(event: schemas.event, db: Session=Depends(get_db)):
     return crud.create_event(db=db, event=event)
 
-#DB_READ
+
+# DB_READ
 @app.get("/users", response_model=List[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(skip: int=0, limit: int=100, db: Session=Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
+
 @app.get("/events", response_model=List[schemas.Event])
-async def read_events(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_events(skip: int=0, limit: int=100, db: Session=Depends(get_db)):
     events = crud.get_events(db, skip=skip, limit=limit)
-    return events    
+    return events
 
 
 @app.get("/")
