ce1666bbc3fd76b870c8550555292858c36e470c
t-east <azmtomya.a127@gmail.com>
2021-12-03 00:49:06 +0900

    add: usecases.event

---
+++ app/usecases/event.py
@@ -0,0 +1,52 @@
+import abc
+from typing import Any, Dict, Optional, Union
+
+import app.domains.entities as entities
+
+
+class IEventRepository(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def get(self, id: int, user_id: int) -> Optional[entities.Event]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def create(self, obj_in: entities.EventCreate) -> entities.Event:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def update(
+        self, id: int, user_id: int, obj_in: Union[entities.EventUpdate, Dict[str, Any]]
+    ) -> Optional[entities.Event]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def delete(self, id: int, user_id: int) -> Optional[entities.Event]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_list(self, user_id: int) -> entities.ListEventsResponse:
+        raise NotImplementedError
+
+
+class EventUsecase:
+    repo: IEventRepository
+
+    def __init__(self, repo: IEventRepository) -> None:
+        self.repo = repo
+
+    def get(self, id: int, user_id: int) -> Optional[entities.Event]:
+        return self.repo.get(id=id, user_id=user_id)
+
+    def create(self, obj_in: entities.EventCreate) -> entities.Event:
+        return self.repo.create(obj_in=obj_in)
+
+    def update(
+        self, id: int, user_id: int, obj_in: Union[entities.EventUpdate, Dict[str, Any]]
+    ) -> Optional[entities.Event]:
+        return self.repo.update(id=id, user_id=user_id, obj_in=obj_in)
+
+    def delete(self, id: int, user_id: int) -> Optional[entities.Event]:
+        return self.repo.delete(id=id, user_id=user_id)
+
+    def get_list(self, user_id: int) -> entities.ListEventsResponse:
+        return self.repo.get_list(user_id=user_id)
