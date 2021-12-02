76365f9e90edfd4f3611e16b5cc913bb5943396f
t-east <azmtomya.a127@gmail.com>
2021-12-03 02:03:51 +0900

    add: interface.SQLRepository db

--- app/interfaces/user.py
+++ app/interfaces/user.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, Optional, Union
 
 # from fastapi.encoders import jsonable_encoder
-# from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session
 
 import app.domains.entities as entities
 import app.drivers.rdb.models as models
@@ -12,7 +12,7 @@ import app.usecases as usecases
 
 # ここで記述処理は，型の変換と最小限のエラー処理．メインロジックはusecaseが担当するのであまり余計な事は書かない．
 class SQLUserRepository(usecases.IUserRepository):
-    def __init__(self) -> None:
+    def __init__(self, db: Session) -> None:
         pass
 
     def _get(self, id: int) -> Optional[models.User]:
@@ -21,11 +21,8 @@ class SQLUserRepository(usecases.IUserRepository):
     def get(self, id: int) -> Optional[entities.User]:
         pass  # _getで得たmodelsをentities.Userで出力
 
-    def _get_by_email(self, email: str) -> Optional[models.User]:
+    def _get_email(self, email: str) -> Optional[models.User]:
         pass  # このクラス内でのみ使うメールアドレス検索関数．modelの型からentitiesの型にする前準備の関数として切り離しておく
-
-    def get_by_email(self, email: str) -> Optional[entities.User]:
-        pass  # _get_by_emailで得たmodelsをentities.Userで出力
 
     def create(self, obj_in: entities.UserCreate) -> entities.User:
         pass  # ユーザ登録の処理で入力したentities.UserCreateの型をmodelsの型にしてdbに保存．その後取り出したものをentities.Userになおして出力
@@ -38,7 +35,7 @@ class SQLUserRepository(usecases.IUserRepository):
     def delete(self, id: int) -> Optional[entities.User]:
         pass  # 入力されたidのDBを削除，削除するデータをentities.Userにして出力
 
-    def authenticate(self, email: str, password: str) -> Optional[entities.User]:
+    def authenticate(self, auth_in: entities.UserAuth) -> Optional[entities.User]:
         pass
         # 入力されたメールアドレス，パスワードを用いてDBの値と一致しているか確認する．
         # _get_by_emailを使って得たhashedpassとpasswordをハッシュ化したものを比較して正しければentities.Userを出力
