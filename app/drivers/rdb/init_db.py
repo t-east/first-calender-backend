95184d9749dff101d76bec55d8e754c80ffcfe32
t-east <azmtomya.a127@gmail.com>
2022-01-30 01:21:27 +0900

    初期データ生成

--- app/drivers/rdb/init_db.py
+++ app/drivers/rdb/init_db.py
@@ -13,12 +13,6 @@ def create_user(db: Session, data: dict) -> None:
     uu = deps.get_user_usecase(db)
     uu.create(obj_in)
 
-    obj_in_db = db.query(models.User).get(data["id"])
-    obj_in_db.created_at += datetime.timedelta(days=-2, hours=data["id"])
-    db.add(obj_in_db)
-    db.commit()
-    db.refresh(obj_in_db)
-
 
 def init_db(db: Session, fixtures: List[Dict[str, Any]]) -> None:
     for data in fixtures:
