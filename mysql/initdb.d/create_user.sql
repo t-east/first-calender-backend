335af4322de4bcccaf62628f2ca6ce4b0b57d22d
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-01-30 20:34:57 +0900

    不要なSQLファイル削除

--- mysql/initdb.d/create_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(30) NOT NULL,
-    email VARCHAR(128) NOT NULL,
-    birthday DATE NULL,
-    PRIMARY KEY (id)
-);
--- mysql/initdb.d/init_test.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO test_user (name, email) VALUES ("tanaka taro", "tanaka.taro@example.com");
-INSERT INTO test_user (name, email) VALUES ("yamada hanako", "yamada.hanako@example.com");
