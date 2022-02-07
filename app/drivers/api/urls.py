70731e4b23ae5fb422aa5d048f3340b67e375220
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-02-07 18:25:06 +0900

    create tag url and deps

--- app/drivers/api/urls.py
+++ app/drivers/api/urls.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from app.drivers.api.endpoints import (
     user,
     event,
+    tag
 )
 
 api_router = APIRouter()
 
 api_router.include_router(user.router, prefix="/api/user", tags=["user"])
 api_router.include_router(event.router, prefix="/api/event", tags=["event"])
+api_router.include_router(tag.router, prefix="/api/tag", tags=["tag"])
