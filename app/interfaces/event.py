728aacf598fda6fcabd347aafc75c1f7b430c8ac
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-02-07 20:26:27 +0900

    add validate error in event update

--- app/interfaces/event.py
+++ app/interfaces/event.py
@@ -90,10 +90,10 @@ class SQLEventRepository(usecases.IEventRepository):
         get_event_model = self._find_event(event_id)
         if not get_event_model:
             raise HTTPException(status_code=404, detail="指定されたイベントは存在しません")
-
         for var, value in vars(obj_in).items():
             setattr(get_event_model, var, value) if value else None
-
+        if self.is_validate_date(get_event_model.from_date, get_event_model.to_date):
+            raise HTTPException(status_code=400, detail="invalid date")
         get_event_model.updated_at = datetime.datetime.now()
         self.db.add(get_event_model)
         self.db.commit()
