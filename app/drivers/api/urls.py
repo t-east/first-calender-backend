82a3588cfda140e115bdcb8df61604e6ee077743
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-02-20 02:17:27 +0900

    Restful APIの記事見て，複数形の方がいいとわかったので修正．フロントエンドも変わるかと思います

--- app/drivers/api/urls.py
+++ app/drivers/api/urls.py
@@ -4,6 +4,6 @@ from app.drivers.api.endpoints import user, event, tag
 
 api_router = APIRouter()
 
-api_router.include_router(user.router, prefix="/api/user", tags=["user"])
-api_router.include_router(event.router, prefix="/api/event", tags=["event"])
-api_router.include_router(tag.router, prefix="/api/tag", tags=["tag"])
+api_router.include_router(user.router, prefix="/api/users", tags=["user"])
+api_router.include_router(event.router, prefix="/api/events", tags=["event"])
+api_router.include_router(tag.router, prefix="/api/tags", tags=["tag"])
